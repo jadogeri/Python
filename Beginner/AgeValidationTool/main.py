552540b2e57bc96953705375e41f29defe7f4b56
OrleansParishIT <jadogeri@uno.edu>
2023-11-21 16:01:32 -0600

    added test case

--- Beginner/AgeValidationTool/main.py
+++ Beginner/AgeValidationTool/main.py
@@ -2,13 +2,22 @@ drinking_age = 21;
 club_age = 18;
 run = True;
 
+if __name__ == "__main__":
+    print("the main prog")
+else:
+    print("not the main prog")
+
 def validateAge(age):
     if (age <= club_age):
         print("Not old enough to get into the club\n");
+        return age;
     elif(age > club_age and age < drinking_age):
         print("You can come in but cant buy a drink\n");
+        return age;
     else:
         print("You can come in and buy a drink\n");
+        return age;
+
 while (run) :
     age = int(input("how old are you\n"))
     validateAge(age);
---
+++ Beginner/AgeValidationTool/test.py
@@ -0,0 +1,34 @@
+import unittest
+
+class unitTest(unittest.TestCase):
+    # Arrange 
+    num1 = 10
+    num2 = 15
+    num3 = 17
+    num4 = 18
+    num5 = 20
+    num6 = 21
+    num7 = 25
+
+    # Act
+    
+
+    # Assert
+    def test_test(self):
+        pass;
+    def test_test2(self):
+        pass;
+
+class unitTest2(unittest.TestCase):
+    def test_test(self):
+        pass;
+    def test_test2(self):
+        pass;
+
+
+    
+    
+
+
+if __name__ == "__main__":
+    unittest.main()
