e19bf7df381022a90fc031236819a6a54cc0bc4c
OrleansParishIT <jadogeri@uno.edu>
2023-12-21 13:47:04 -0600

    modified main.py

--- Beginner/StudentGrader/src/main.py
+++ Beginner/StudentGrader/src/main.py
@@ -1,38 +1,63 @@
 from constants import *
+from utils.utils import *
 
-drinking_age = 21;
-club_age = 18;
+
 run = True;
 
+test = 0
+lab = 0
+homework = 0
+user_score = 0
 
-
-# if __name__ == "__main__":
-#     print("the main prog")
-# else:
-#     print("not the main prog")
-
-def validateAge(age):
-    response = "";
-    if (age < club_age):
-        print(MINOR_AGE_PROMPT);
-        response = MINOR_AGE_PROMPT
-        return response;
-    elif(age >= club_age and age < drinking_age):
-        print(CLUB_AGE_PROMPT);
-        response = CLUB_AGE_PROMPT
-        return response;
-    else:
-        print(ADULT_AGE_PROMPT);
-        response = ADULT_AGE_PROMPT
-        return response;
+test_percentage     = 0.4
+lab_perentage       = 0.1
+homework_percentage = 0.5
 
 while (run and __name__ == "__main__") :
     try:
-        age = int(input("how old are you\n"))
-        validateAge(age);
+        counter = 3
+        while(counter > 0):
+            user_score = int(input());
+            if is_valid_score(user_score):
+                match counter:
+                    case 3:
+                        test = get_test_percentage_score(user_score)
+                        print("test == ",test,"counter ==",counter)
+                        counter = counter - 1
+                    case 2:
+                        lab = get_lab_percentage_score(user_score)                       
+                        print("lab == ",lab,"counter ==",counter)
+                        counter = counter - 1
+                    case 1:
+                        homework = get_homework_percentage_score(user_score)
+                        print("homework == ",homework,"counter ==",counter)
+                        counter = counter - 1                  
+                    case default :
+                        break
+        total = test + lab + homework  
+        print("total ==",total,"/100")     
+           
     except ValueError as err :
         print(err)
     option = str(input('Do you want to try again!! Y or y for yes and N or n a no\n'))
-    if option.upper() != "Y":
+    if option.upper() == "Y":
+        run = True;
+        print(" this is true")
+        counter = 3
+    elif option.upper() == "N":
         run = False;
+        print(" this is false")
+       
  
+
+
+
+# def get_round_count():
+#     total_rounds = 0
+#     try:
+#         count = int(input("how many rounds\n"))  
+#         total_rounds = count      
+#     except ValueError as err :
+#         print(err)
+#     return total_rounds
+    
---
+++ Beginner/StudentGrader/src/utils/utils.py
@@ -0,0 +1,15 @@
+def is_valid_score(value):
+    if(value >= 0 or value <= 100):
+        return True
+    else:
+        return False
+
+def get_test_percentage_score(value):
+    return 0.4 * value
+
+def get_lab_percentage_score(value):
+    return 0.1 * value
+
+
+def get_homework_percentage_score(value):
+    return 0.5 * value
