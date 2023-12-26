e4f82421212484c1170fc6c4de260ee15b55a635
OrleansParishIT <jadogeri@uno.edu>
2023-12-26 11:05:06 -0600

    fixed bugs in main.py

--- Beginner/StudentGrader/src/main.py
+++ Beginner/StudentGrader/src/main.py
@@ -1,4 +1,3 @@
-from constants import *
 from utils.utils import *
 
 
@@ -21,43 +20,23 @@ while (run and __name__ == "__main__") :
             if is_valid_score(user_score):
                 match counter:
                     case 3:
-                        test = get_test_percentage_score(user_score)
-                        print("test == ",test,"counter ==",counter)
+                        test = get_test_percentage_score(user_score)                        
                         counter = counter - 1
                     case 2:
-                        lab = get_lab_percentage_score(user_score)                       
-                        print("lab == ",lab,"counter ==",counter)
+                        lab = get_lab_percentage_score(user_score)                 
                         counter = counter - 1
                     case 1:
                         homework = get_homework_percentage_score(user_score)
-                        print("homework == ",homework,"counter ==",counter)
                         counter = counter - 1                  
                     case default :
                         break
-        total = test + lab + homework  
-        print("total ==",total,"/100")     
+        total = test + lab + homework          
            
     except ValueError as err :
         print(err)
     option = str(input('Do you want to try again!! Y or y for yes and N or n a no\n'))
-    if option.upper() == "Y":
-        run = True;
-        print(" this is true")
-        counter = 3
-    elif option.upper() == "N":
-        run = False;
-        print(" this is false")
+   
        
  
 
 
-
-# def get_round_count():
-#     total_rounds = 0
-#     try:
-#         count = int(input("how many rounds\n"))  
-#         total_rounds = count      
-#     except ValueError as err :
-#         print(err)
-#     return total_rounds
-    
