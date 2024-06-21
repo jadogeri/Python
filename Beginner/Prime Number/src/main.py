d9e09f584befcf54802800b4af37d1ce30c907bd
jadogeri <jadogeri@uno.edu>
2024-06-21 07:50:13 -0500

    Fixed bugs in Prime Number directory

--- Beginner/Prime Number/src/main.py
+++ Beginner/Prime Number/src/main.py
@@ -5,29 +5,89 @@ from constants import *
 #     print("the main prog")
 # else:
 #     print("not the main prog")
-
-def validateAge(age):
-    response = "";
-    if (age < club_age):
-        print(MINOR_AGE_PROMPT);
-        response = MINOR_AGE_PROMPT
-        return response;
-    elif(age >= club_age and age < drinking_age):
-        print(CLUB_AGE_PROMPT);
-        response = CLUB_AGE_PROMPT
-        return response;
-    else:
-        print(ADULT_AGE_PROMPT);
-        response = ADULT_AGE_PROMPT
-        return response;
-
-while (run and __name__ == "__main__") :
+input_count : int = 0
+while (input_count == 0):
     try:
-        age = int(input("how old are you\n"))
-        validateAge(age);
-    except ValueError as err :
+        input_count = int(input("how many inputs?\n"));        
+
+    except  ValueError as err :
         print(err)
-    option = str(input('Do you want to try again!! Y or y for yes and N or n a no\n'))
-    if option.upper() != "Y":
-        run = False;
+
+def isPrimeNumber(user_choice : int):
+    
+    outcome = False;
+    exponent = 0;
+    base = 2;
+
+    if (user_choice == 1 or user_choice == 0):
+        outcome = False;
+        return outcome;    
+    
+    else:    
+        while (base ** exponent < user_choice ): 
+            if ((base ** exponent) + 1 == user_choice ):                
+                outcome = True;
+                return outcome;
+
+            else:
+                exponent += 1;
+        return outcome;
+
+
+while (input_count > 0 and __name__ == "__main__") :
+    try:
+        input_string = str(input("Enter numner?\n"));
+        input_int = int(input_string);
+
+        if(isPrimeNumber(input_int)):
+            print("True");
+        else:
+            print("False");
+        input_count -= 1
+    except ValueError as err :
+        print(err);
+   
  
+"""
+
+import time
+import random
+
+
+from utils.utils import *
+
+choices = []
+
+rounds : int = 0;
+
+try:
+    file = open('constants.txt')
+    cpu_choices = file.readlines();
+    file.close();
+    rock, paper ,scissors = cpu_choices ;
+    round_text = input("how many rounds\n");
+
+    rounds = get_round_count(round_text);
+
+except  ValueError as err :
+    print(err)
+
+    
+
+
+
+try:
+        player_choice = input("TYPE [ROCK], [PAPER] OR [SCISSORS]\n");
+        if (is_valid_entry(player_choice)) :
+            
+            cpu_choice = random.choice(cpu_choices);
+            print("cpu chose "+ cpu_choice);
+            outcome = get_round_outcome(player_choice,cpu_choice);
+            time.sleep(3);
+            print(outcome);
+            rounds -= 1;
+        else:
+            print("ENTRY '",player_choice,"' IS NOT A VALID INPUT");
+
+
+"""
