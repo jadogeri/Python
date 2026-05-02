cc1476a5b9f80ae7c3361aa28b1f4447e47ff579
joseph adogeri <josephadogeridev@gmail.com>
2026-05-02 18:54:00 -0500

    Added strip()

--- Beginner/AreaOfASquare/src/main.py
+++ Beginner/AreaOfASquare/src/main.py
@@ -10,7 +10,7 @@ run = True;
 # else:
 #     print("not the main prog")
 
-def convert_string_to_number(value):
+def convert_string_to_number(value) -> int | float:
     radius = int | float ;
     if value.isdigit() :
         radius = int(value)
@@ -25,13 +25,13 @@ def calculate_area(radius):
 
 while (run and __name__ == "__main__") :
     try:
-        radius_string = (input(ENTER_VALUE_PROMPT));
+        radius_string : str = (input(ENTER_VALUE_PROMPT));
 
         radius = convert_string_to_number(radius_string);
         print(calculate_area(radius));
     except ValueError as err :
         print(err)
-    option = str(input('Do you want to try again!! Y or y for yes and N or n a no\n'))
-    if option.upper() != "Y":
+    option : str = input('Do you want to try again!! Y or y for yes and N or n a no\n')
+    if option.upper().strip() != "Y":
         run = False;
  
